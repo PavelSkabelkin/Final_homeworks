67a3aabc3cbcc5ea63fc247c14b4b064bef5ac1f
PavelSkabelkin <145216683+PavelSkabelkin@users.noreply.github.com>
2023-10-12 21:12:36 +0300

    Add files via upload

--- task 2.py
+++ task 2.py
@@ -1,32 +1,60 @@
-def remember(func):
-    cache = {}
+class LRUCache:
+    def __init__(self, capacity):
+        self.capacity = capacity  # Вместимость кэша
+        self.cache_dict = {}  # Словарь для хранения элементов кэша
 
-    def wrapper(*args, **kwargs):
-        key = (args, frozenset(kwargs.items()))
-        if key not in cache:
-            cache[key] = func(*args, **kwargs)
-            print(f"Кэширован результат для {args}")
-        else:
-            print(f"Использован кэшированный результат для {args}")
-        return cache[key]
+    @property
+    def cache(self):
+        least_recently_used = next(iter(self.cache_dict))
+        # Получение ключа самого старого элемента
+        return (least_recently_used, self.cache_dict[least_recently_used])
+        # Возвращение кортежа (ключ, значение)
 
-    return wrapper
+    @cache.setter
+    def cache(self, new_elem):
+        key, value = new_elem
+        # Распаковка нового элемента
+        if key in self.cache_dict:
+            # Если ключ уже есть в кэше
+            del self.cache_dict[key]
+            # Удаляем элемент, чтобы обновить его положение
+        elif len(self.cache_dict) == self.capacity:
+            # Если превышено ограничение вместимости кэша
+            del self.cache_dict[next(iter(self.cache_dict))]
+            # Удаляем самый старый элемент
+        self.cache_dict[key] = value
+        # Добавляем новый элемент в кэш
 
-@remember
-def fibonacci(n):
-    if n <= 0:
-        return "Пожалуйста, введите положительное целое число."
+    def get(self, key):
+        if key in self.cache_dict:
+            # Если ключ есть в кэше
+            value = self.cache_dict[key]
+            # Получаем значение
+            del self.cache_dict[key]
+            # Удаляем элемент
+            self.cache_dict[key] = value
+            # Добавляем элемент обратно в кэш
+            return value
+            # Возвращаем значение
 
-    fib = [0, 1]
-    for i in range(2, n + 1):
-        fib.append(fib[i - 1] + fib[i - 2])
+    def print_cache(self):
+        print("LRU Cache:")
+        for key, value in self.cache_dict.items():
+            # Перебираем элементы кэша
+            print(f"{key} : {value}")
+            # Выводим ключ и значение
 
-    return fib[n]
 
-try:
-    n = int(input("Введите номер числа Фибоначчи: "))
-    result = fibonacci(n)
-    print(f"Число Фибоначчи с номером {n}: {result}")
+# Пример использования
+cache = LRUCache(3)
 
-except ValueError:
-    print("Пожалуйста, введите целое число.")
+cache.cache = ("key1", "value1")
+cache.cache = ("key2", "value2")
+cache.cache = ("key3", "value3")
+
+cache.print_cache()
+print(cache.get("key2"))
+
+cache.cache = ("key4", "value4")
+
+cache.print_cache()
